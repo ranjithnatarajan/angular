2a435e0dcecad717535b6d603f6a249fac2f42e8
Natarajan, Ranjith <ranjithnatarajan@users.noreply.github.com>
2019-02-20 17:43:58 +0530

    editing styles; styleUrls take over styles when both are in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss'],
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }`]
 })
 export class AppComponent {
 }
